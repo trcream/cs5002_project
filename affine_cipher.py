2c036bff8f667c1e93f4e1f060b2f5786e10e279
Tcreamer <trentoncreamer@yahoo.com>
2021-12-15 15:59:31 -0800

    Cipher and project finished

--- affine_cipher.py
+++ affine_cipher.py
@@ -2,9 +2,18 @@ ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 
 def encrypt_affine(a_key, b_key, text_to_encrypt):
-    print('A Key', a_key)
-    print('B Key', b_key)
-    print('Cipher Text', text_to_encrypt)
+    '''
+    Function -- encrypt_affine
+        Function to take a string from the user and encrypt it
+    
+    Parameter:
+        a_key -- First key entered by the user
+        b_key -- Second key entered by the user
+        text_to_encrypt -- String input from the user to be encrypted
+    
+    Returns:
+        Returns the string envcrpted with the cipher
+    '''
     text_to_encrypt_index_list = []
     encrypted_index_value = ''
     encrypted_index_values_list = []
@@ -12,74 +21,74 @@ def encrypt_affine(a_key, b_key, text_to_encrypt):
     
     # Index values of the text to encrypt
     for character in text_to_encrypt:
-        #print(character)
         if character in ALPHABET:
             text_to_encrypt_index_list.append(ALPHABET.index(character))
     print('Text to encrypt Index List', text_to_encrypt_index_list)
 
     # Index values encrypted to new index values
     for index in text_to_encrypt_index_list:
-        print(index)
         encrypted_index_value = ((a_key * index) + b_key) % 26
-        print(encrypted_index_value)
         encrypted_index_values_list.append(encrypted_index_value)
     print(encrypted_index_values_list)
 
     # Converting the decrypted values to the alphabet 
     for i in encrypted_index_values_list:
-        print('Encrypted Index Values', i)
         encrypted_index_letter = ALPHABET[i]
-        print(encrypted_index_letter)
         encrypted_index_letter_list.append(ALPHABET[i])
     print(encrypted_index_letter_list)
+    return encrypted_index_letter_list
 
 
 def decrypt_affine(a_key, b_key, encrypted_text, inverse_mod_a):
     '''
-    Decrypt x = NOT A * (Y - B) mod 26
-
-    A = Key 1
-    B - Key 2
-    Y - Encrypted Cipher Text
-
-    NOT A = Modular Multiplicative Inverse
+    Function -- decrypt_affine
+        Function to take a string from the user and encrypt it
+    
+    Parameter:
+        a_key -- First key entered by the user
+        b_key -- Second key entered by the user
+        encrypted_text -- Text string to be encrypted
+        inverse_mod_a -- Inverse modulo which is calculated from another function
+    
+    Returns:
+        Returns the string envcrpted with the cipher
 
     '''
     decrypted_index_list = []
     decrypted_index_value = ''
     decrypted_letter_values = []
     encrypted_index_list = []
-    print(ALPHABET[0])
-    print('A Key', a_key)
-    print('B Key', b_key)
-    print("Encrypted Text", encrypted_text)
-    print('Inverse Mod A', inverse_mod_a)
 
     # Converting the encrypted characters to an index list 
     for character in encrypted_text:
-        # print(character)
         if character in ALPHABET:
-            # print(ALPHABET.index(character))
             encrypted_index_list.append(ALPHABET.index(character))
     print('Encrypted Index List', encrypted_index_list)
 
     # Converting the encrypted index values to decrypted index values
     for index in encrypted_index_list:
         decrypted_index_value = (inverse_mod_a * (index - b_key)) % 26 
-        # print('Decrypted Index Value', decrypted_index_value)
         decrypted_index_list.append(decrypted_index_value)
-    print(decrypted_index_list)
 
     # Converting the decrypted index values back to characters
     for i in decrypted_index_list:
-        # print(i)
         decrypted_index_letter = ALPHABET[i]
         decrypted_letter_values.append(decrypted_index_letter)
     print(decrypted_letter_values)
-
+    return decrypted_letter_values
+    
 
 def inverse_modulo(a_key):
-    # print('A Key', a_key)
+    '''
+    Function -- inverse_modulo
+        Function to calcuate the modular multiplicative inverse of the A key
+    
+    Parameter:
+        a_key -- First key entered by the user
+
+    Returns:
+        Returns the inverse modulo of the A key
+    '''
     modular_inverse = 0
     inverse_mod_result = 0
     while inverse_mod_result !=1:
@@ -89,23 +98,34 @@ def inverse_modulo(a_key):
             inverse_mod_result = (a_key * modular_inverse) % 26
         else:
             print('There is no modular inverse')
-            break
+            return False
     print('Modular Inverse', modular_inverse)
     return modular_inverse
 
 
 def main():
-    a_key = 7
-    b_key = 10
-    text_to_encrypt = input('Please enter the text you would like to encrypt ').lower()
-    encrypt_affine(a_key, b_key, text_to_encrypt)
-    inverse_mod_a = int(inverse_modulo(a_key))
+    a_key = int(input('What would you like the A key to be?'))
+    # Error handling for the A key to make sure it is invertible
+    inverse_mod_a = inverse_modulo(a_key)
+    if inverse_mod_a != 0:
+        b_key = int(input('What would you like the B key to be?'))
+        user_option = int(input('Please choose from:\n1: Encryption\n2: Decryption\n3: quit\n'))
 
-    # LJMKGMGMXFQEXMW
-    encrypted_text = input('Please enter the text to be decrypted ').lower()
-    decrypt_affine(a_key, b_key, encrypted_text, inverse_mod_a)
+        while user_option !=3: 
+            if user_option == 1:
+                #Encryption with Affine 
+                text_to_encrypt = input('Please enter the text you would like to encrypt ').lower()
+                encrypt_affine(a_key, b_key, text_to_encrypt)
+                user_option = int(input('Please choose from:\n1: Encryption\n2: Decryption\n3: quit\n'))
 
-
-
+            elif user_option == 2:
+                # Decryption with Affine
+                # Example: LJMKGMGMXFQEXMW
+                encrypted_text = input('Please enter the text to be decrypted ').lower()
+                decrypt_affine(a_key, b_key, encrypted_text, inverse_mod_a)
+                user_option = int(input('Please choose from:\n1: Encryption\n2: Decryption\n3: quit\n'))
+    else:
+        print('Please select a new A value. There is no modular multiplicative inverse for this integer\n')
+    
 if __name__ == '__main__':
     main()
